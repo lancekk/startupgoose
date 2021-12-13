c87fd908aee30263eb8db141f0b1c8d39fb6a818
Lance Kelley <lancekkcodes@gmail.com>
2021-12-12 19:52:47 -0600

    Thoughts without reactions

--- models/Thought.js
+++ models/Thought.js
@@ -5,8 +5,21 @@ const ThoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      // length
+      minlength: 1,
+      maxlength: 280,
     },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: fmtDate,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: {
+      // TODO
+    }
   },
   {}
 );
